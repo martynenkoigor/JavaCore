85aa20b569c8ea2c24996da364a589fbe8ccfb25
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-16 15:06:48 +0200

    Add class DynInit

---
+++ src/main/com/martynenkoigor/javacore/chapter02/DynInit.java
@@ -0,0 +1,10 @@
+package com.martynenkoigor.javacore.chapter02;
+
+public class DynInit {
+    public static void main(String[] args) {
+        double a = 3.0, b = 4.0;
+
+        double c = Math.sqrt(a * a + b * b);
+        System.out.println("Гипoтeнyзa равна " + c );
+    }
+}
