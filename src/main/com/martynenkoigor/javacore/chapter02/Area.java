e7e319e692c407f8bc2f254adcd2b701a4fe6472
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-16 13:19:09 +0200

    Add class Area

---
+++ src/main/com/martynenkoigor/javacore/chapter02/Area.java
@@ -0,0 +1,17 @@
+package com.martynenkoigor.javacore.chapter02;
+
+// Вычислить площадь круга
+
+public class Area {
+    public static void main(String[] args) {
+      double pi, r, s;
+
+      r = 10.8;
+      pi = 3.1416;
+      s = pi * r * r;
+
+        System.out.println("Плoщaдь круга равна " + s );
+
+
+    }
+}
