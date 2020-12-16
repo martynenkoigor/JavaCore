0c08c183fde39002ac0cd04d44ce0bf6710063dd
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-16 14:22:03 +0200

    Add class Booltest

---
+++ src/main/com/martynenkoigor/javacore/chapter02/BoolTest.java
@@ -0,0 +1,18 @@
+package com.martynenkoigor.javacore.chapter02;
+
+public class BoolTest {
+    public static void main(String[] args) {
+        boolean b;
+
+        b = false;
+        System.out.println("b равно " + b );
+        b = true;
+        System.out.println("b равно " + b );
+
+        if (b) System.out.println("Этoт код выполняется . ");
+        b = false;
+        if (b) System.out.println("Этoт код не выполняется.");
+
+        System.out.println("1O > 9 равно " + (10 > 9));
+    }
+}
