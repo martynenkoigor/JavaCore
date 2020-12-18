afae91d7980ed091817ee69821f35980e1986090
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-18 15:30:45 +0200

    Add class Ternary

---
+++ src/main/com/martynenkoigor/javacore/chapter03/Ternary.java
@@ -0,0 +1,19 @@
+package com.martynenkoigor.javacore.chapter03;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int i, k;
+
+        i = 10;
+        k = i < 0 ? -i : i;
+        System.out.print("Aбcoлютнoe значение ");
+        System.out.println(i + " равно " + k );
+
+        i = -10;
+        k = i < 0 ? -i : i;
+        System.out.print("Aбcoлютнoe значение ");
+        System.out.println(i + " равно " + k );
+
+
+    }
+}
