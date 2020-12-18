07ce19468177c1d5b0e82085eabe7768ba04b57d
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-18 12:41:07 +0200

    Add class ByteShift

---
+++ src/main/com/martynenkoigor/javacore/chapter03/ByteShift.java
@@ -0,0 +1,17 @@
+package com.martynenkoigor.javacore.chapter03;
+
+public class ByteShift {
+    public static void main(String[] args) {
+        byte a = 64, b;
+        int i;
+
+        i = a << 2;
+        b = (byte) (a << 2);
+
+        System.out.println("Пepвoнaчaльнoe значение а : " +  a );
+        System.out.println("i and b: " + i + " " + b );
+
+
+
+    }
+}
