8d9579d116db8846fb41e19cb9347ca0f999272f
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-21 15:41:47 +0200

    Add class NoBody

---
+++ src/main/com/martynenkoigor/javacore/chapter04/NoBody.java
@@ -0,0 +1,14 @@
+package com.martynenkoigor.javacore.chapter04;
+
+public class NoBody {
+    public static void main(String[] args) {
+        int i, j;
+
+        i = 100;
+        j = 200;
+
+        while (++i < --j);
+
+        System.out.println("Cpeднee значение равно " + i );
+    }
+}
