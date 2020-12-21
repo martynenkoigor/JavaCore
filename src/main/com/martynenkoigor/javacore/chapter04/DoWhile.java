d23409ffa6adf093694a987e7f80cc3af68dbdbb
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-21 15:48:46 +0200

    Add class DoWhile

---
+++ src/main/com/martynenkoigor/javacore/chapter04/DoWhile.java
@@ -0,0 +1,11 @@
+package com.martynenkoigor.javacore.chapter04;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int n = 10;
+
+        do {
+            System.out.println("тaкт " + n );
+        } while (--n > 0);
+    }
+}
