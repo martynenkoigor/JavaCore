844660679a1df007b90f40f400d363b6632353ce
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-22 16:58:34 +0200

    Add class Break

--- src/main/com/martynenkoigor/javacore/chapter04/Break.java
+++ src/main/com/martynenkoigor/javacore/chapter04/Break.java
@@ -1,4 +1,20 @@
 package com.martynenkoigor.javacore.chapter04;
 
 public class Break {
+    public static void main(String[] args) {
+        boolean t = true;
+
+        first: {
+            second: {
+              third: {
+                  System.out.println("Пpeдшecтвyeт оператору break.");
+                  if (t) break second;
+                  System.out.println("Этoт оператор не будет выполняться");
+
+             }
+                System.out.println("Этoт оператор не будет выполняться");
+           }
+            System.out.println("Этoт оператор не будет выполняться");
+        }
+     }
 }
