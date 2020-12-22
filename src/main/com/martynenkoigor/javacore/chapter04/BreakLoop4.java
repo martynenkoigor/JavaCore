05a813520ea0c5f1288452fe1f2ed1d11178ebf7
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-22 16:57:51 +0200

    Add class BreakLoop4

---
+++ src/main/com/martynenkoigor/javacore/chapter04/BreakLoop4.java
@@ -0,0 +1,16 @@
+package com.martynenkoigor.javacore.chapter04;
+
+public class BreakLoop4 {
+    public static void main(String[] args) {
+        outer: for (int i = 0; i < 3; i++) {
+            System.out.println("Пpoxoд " + i + ": ");
+            for (int j = 0; j < 100; j++) {
+                if (j == 10) break outer;
+                System.out.print(j + " ");
+            }
+            System.out.println("Этa строка не будет выводиться");
+
+        }
+        System.out.println("Циклы завершены.");
+    }
+}
