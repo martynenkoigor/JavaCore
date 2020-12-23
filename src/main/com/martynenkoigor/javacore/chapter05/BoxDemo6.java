d525f1d4dc665a956719b373460d1c4327bade1c
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-23 16:27:01 +0200

    Add class BoxDemo6

---
+++ src/main/com/martynenkoigor/javacore/chapter05/BoxDemo6.java
@@ -0,0 +1,16 @@
+package com.martynenkoigor.javacore.chapter05;
+
+public class BoxDemo6 {
+    public static void main(String[] args) {
+        Box mybox1 = new Box();
+        Box mybox2 = new Box();
+
+        double vol;
+
+        vol = mybox1.volume();
+        System.out.println("Oбъeм равен " + vol);
+
+        vol = mybox2.volume();
+        System.out.println("Oбъeм равен " + vol);
+    }
+}
