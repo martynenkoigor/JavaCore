e314e0ce38c193ba59b0d5361e34e47d259d0c47
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-23 14:06:31 +0200

    Add class Box

---
+++ src/main/com/martynenkoigor/javacore/chapter05/Box.java
@@ -0,0 +1,24 @@
+package com.martynenkoigor.javacore.chapter05;
+
+public class Box {
+    double width;
+    double height;
+    double depth;
+}
+
+
+class BoxDemo {
+    public static void main(String[] args) {
+        Box mybox = new Box();
+
+        double vol;
+
+        mybox.width = 10;
+        mybox.height = 20;
+        mybox.depth = 15;
+
+        vol = mybox.depth * mybox.height * mybox.width;
+
+        System.out.println("Oбъeм равен " + vol);
+    }
+}
