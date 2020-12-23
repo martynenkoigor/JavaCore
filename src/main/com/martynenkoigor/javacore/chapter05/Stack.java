d1c03c39f17128cd83e9881e0691a51410b3060e
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-23 17:33:29 +0200

    Add class Stack

--- src/main/com/martynenkoigor/javacore/chapter05/Stack.java
+++ src/main/com/martynenkoigor/javacore/chapter05/Stack.java
@@ -1,4 +1,28 @@
 package com.martynenkoigor.javacore.chapter05;
 
+
 public class Stack {
+    int stck[] = new int[10];
+    int tos;
+
+    Stack() {
+        tos = -1;
+    }
+
+    void push(int item) {
+        if (tos == 9)
+            System.out.println("Cтeк заполнен.");
+        else
+            stck[++tos] = item;
+    }
+
+    int pop() {
+        if (tos < 0) {
+            System.out.println("Cтeк не загружен.");
+            return 0;
+        }
+        else
+            return stck[tos--];
+    }
+
 }
