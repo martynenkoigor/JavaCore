1fa201ba9941e9b11be4f0e5906774a338f4e820
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-23 17:26:34 +0200

    Add class Box

--- src/main/com/martynenkoigor/javacore/chapter05/Box.java
+++ src/main/com/martynenkoigor/javacore/chapter05/Box.java
@@ -5,15 +5,28 @@ public class Box {
     double height;
     double depth;
 
+    Box(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+
     double volume() {
         return width * height * depth;
     }
+
+    void setDim(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+
 }
 
 
 class BoxDemo {
     public static void main(String[] args) {
-        Box mybox = new Box();
+        Box mybox = new Box(10,20,15);
 
         double vol;
 
