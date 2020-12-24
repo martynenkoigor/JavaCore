56e8b5183a7eba87a08fae0f6a882274c5df7bfb
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-24 14:23:18 +0200

    Add class Test

--- src/main/com/martynenkoigor/javacore/chapter06/Test.java
+++ src/main/com/martynenkoigor/javacore/chapter06/Test.java
@@ -1,4 +1,16 @@
 package com.martynenkoigor.javacore.chapter06;
 
 public class Test {
+    int a, b;
+
+    public Test(int i, int j) {
+        a = j;
+        b = j;
+    }
+
+    public boolean equals(Test o) {
+        if (o.a == a && o.b == b) return true;
+        else return false;
+    }
+
 }
