2655f63b40582b48c7766be9116622a2ad072a81
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-24 17:32:32 +0200

    Add class AccessTest

---
+++ src/main/com/martynenkoigor/javacore/chapter06/AccessTest.java
@@ -0,0 +1,14 @@
+package com.martynenkoigor.javacore.chapter06;
+
+public class AccessTest {
+    public static void main(String[] args) {
+        TestModificator ob = new TestModificator();
+
+        ob.a = 10;
+        ob.b = 20;
+        ob.setC(100);
+
+        System.out.println("a, b, и с: " + ob.a + " " + ob.b +
+        " " + ob.getC());
+    }
+}
