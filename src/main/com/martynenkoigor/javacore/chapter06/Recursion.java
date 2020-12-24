1eefe72e972ba107869e6d1593408dfdcb20962f
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-24 16:55:43 +0200

    Add class Recursion

---
+++ src/main/com/martynenkoigor/javacore/chapter06/Recursion.java
@@ -0,0 +1,11 @@
+package com.martynenkoigor.javacore.chapter06;
+
+public class Recursion {
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+
+        System.out.println("Фaктopиaл 3 равен " + f.fact(3));
+        System.out.println("Фaктopиaл 4 равен " + f.fact(4));
+        System.out.println("Фaктopиaл 5 равен " + f.fact(5));
+    }
+}
