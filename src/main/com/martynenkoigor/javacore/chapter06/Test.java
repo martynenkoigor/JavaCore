a4ccefe72cf0c2066c3cfbb9a9b3790476a3f5c5
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-24 17:33:20 +0200

    Add class Test

--- src/main/com/martynenkoigor/javacore/chapter06/Test.java
+++ src/main/com/martynenkoigor/javacore/chapter06/Test.java
@@ -3,14 +3,30 @@ package com.martynenkoigor.javacore.chapter06;
 public class Test {
     int a, b;
 
+    public Test(int i) {
+        a = i;
+    }
+
     public Test(int i, int j) {
-        a = j;
+        a = i;
         b = j;
     }
+
+    public Test incrByTen() {
+        Test temp = new Test(a + 10);
+             return temp;
+    }
+
+
 
     public boolean equals(Test o) {
         if (o.a == a && o.b == b) return true;
         else return false;
+    }
+
+    public void meth(Test o) {
+        o.a *= 2;
+        o.b /= 2;
     }
 
 }
