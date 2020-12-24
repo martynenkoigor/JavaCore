060f41f6a1e095a6b8adae6d31562a7ca2f84bff
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-24 17:37:21 +0200

    Add class TestModificator

--- src/main/com/martynenkoigor/javacore/chapter06/TestModificator.java
+++ src/main/com/martynenkoigor/javacore/chapter06/TestModificator.java
@@ -1,4 +1,15 @@
 package com.martynenkoigor.javacore.chapter06;
 
 public class TestModificator {
+    int a;
+    public int b;
+    private int c;
+
+    void setC(int i) {
+        c = i;
+    }
+
+    int getC() {
+        return c;
+    }
 }
