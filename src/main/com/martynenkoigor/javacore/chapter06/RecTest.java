ef9b285acefba9269a75d061def43f53b688cd49
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-24 17:35:37 +0200

    Add class RecTest

--- src/main/com/martynenkoigor/javacore/chapter06/RecTest.java
+++ src/main/com/martynenkoigor/javacore/chapter06/RecTest.java
@@ -1,4 +1,15 @@
 package com.martynenkoigor.javacore.chapter06;
 
 public class RecTest {
+    int values[];
+
+    RecTest(int i) {
+        values = new int[i];
+    }
+
+    public void printArray(int i) {
+        if (i == 0) return;
+        else printArray(i - 1);
+        System.out.println("[" + (i-1) + "] " + values[i-1]);
+    }
 }
