46a3b0ad415012fb3b8b0d63a52758497aeb558e
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-24 12:01:54 +0200

    Add class Overload

---
+++ src/main/com/martynenkoigor/javacore/chapter06/OverloadDemo.java
@@ -0,0 +1,19 @@
+package com.martynenkoigor.javacore.chapter06;
+
+public class OverloadDemo {
+    void test() {
+        System.out.println("Пapaмeтpы отсутствуют");
+    }
+
+   // void test(int a) {
+   //     System.out.println("a: " + a);
+  //  }
+
+    void test(int a, int b) {
+        System.out.println("a и b: " + a + " " + b);
+    }
+
+    void test(double a) {
+        System.out.println("Внутреннее преобразование при вызове test (double) а: " + a);
+    }
+}
