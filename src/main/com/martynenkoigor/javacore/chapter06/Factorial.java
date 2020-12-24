9d2a76c86df578490f66907e09bfa77985f398bb
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-24 17:34:38 +0200

    Add class Factorial

--- src/main/com/martynenkoigor/javacore/chapter06/Factorial.java
+++ src/main/com/martynenkoigor/javacore/chapter06/Factorial.java
@@ -1,4 +1,11 @@
 package com.martynenkoigor.javacore.chapter06;
 
 public class Factorial {
+   public int fact(int n) {
+        int result;
+
+        if (n == 1) return 1;
+        result = fact(n - 1) * n;
+        return result;
+    }
 }
