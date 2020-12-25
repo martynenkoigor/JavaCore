a5cda4461f829030cec60325db9f7746464acfcb
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-25 13:53:24 +0200

    Add class StaticByName

---
+++ src/main/com/martynenkoigor/javacore/chapter06/StaticByName.java
@@ -0,0 +1,9 @@
+package com.martynenkoigor.javacore.chapter06;
+
+
+public class StaticByName {
+    public static void main(String[] args) {
+        StaticDemo.callme();
+        System.out.println("b = " + StaticDemo.b);
+    }
+}
