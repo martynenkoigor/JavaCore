8e09c1e6531533189644a922510669e2f8d04369
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-25 13:54:27 +0200

    Add class StaticDemo

--- src/main/com/martynenkoigor/javacore/chapter06/StaticDemo.java
+++ src/main/com/martynenkoigor/javacore/chapter06/StaticDemo.java
@@ -1,4 +1,10 @@
 package com.martynenkoigor.javacore.chapter06;
 
 public class StaticDemo {
+    static int a = 42;
+    static int b = 99;
+
+    static void callme() {
+        System.out.println("a = " + a);
+    }
 }
