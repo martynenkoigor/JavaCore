aaa44cb2f60c0629313f936a94df936aeb8e1866
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-25 14:38:26 +0200

    Add class Lenght

---
+++ src/main/com/martynenkoigor/javacore/chapter06/Lenght.java
@@ -0,0 +1,13 @@
+package com.martynenkoigor.javacore.chapter06;
+
+public class Lenght {
+    public static void main(String[] args) {
+        int a1[] = new int[10];
+        int a2[] = {3, 5, 7, 1, 8, 99, 44, -10};
+        int a3[] = {4, 3, 2, 1};
+
+        System.out.println("длинa a1 равна " + a1.length);
+        System.out.println("длинa а2 равна " + a2.length);
+        System.out.println("длинa аЗ равна " + a3.length);
+    }
+}
