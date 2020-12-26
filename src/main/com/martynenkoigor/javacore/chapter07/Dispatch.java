2cba13b547720ab596f9ad3d8357116c9e61c196
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-26 16:46:44 +0200

    Add class Dispatch

--- src/main/com/martynenkoigor/javacore/chapter07/Dispatch.java
+++ src/main/com/martynenkoigor/javacore/chapter07/Dispatch.java
@@ -1,4 +1,20 @@
 package com.martynenkoigor.javacore.chapter07;
 
 public class Dispatch {
+    public static void main(String[] args) {
+         E e = new E();
+         F f = new F();
+         J j = new J();
+
+         E r;
+
+         r = e;
+         r.callme();
+
+         r = f;
+         r.callme();
+
+         r = j;
+         r.callme();
+    }
 }
