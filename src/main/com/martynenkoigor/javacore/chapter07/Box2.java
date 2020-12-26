0a18ec9361e2873bf2f9363f4bdfd76883c81ff0
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-26 14:10:38 +0200

    Add class Box2

--- src/main/com/martynenkoigor/javacore/chapter07/Box2.java
+++ src/main/com/martynenkoigor/javacore/chapter07/Box2.java
@@ -1,4 +1,43 @@
 package com.martynenkoigor.javacore.chapter07;
 
 public class Box2 {
+    double width;
+    double height;
+    double depth;
+
+    public Box2(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+
+    public Box2() {
+        width = -1;
+        height = -1;
+        depth = -1;
+    }
+
+    public Box2(double len) {
+        width = height = depth = len;
+    }
+
+    public Box2(Box2 ob) {
+        width = ob.width;
+        height = ob.height;
+        depth = ob.depth;
+    }
+    double volume() {
+        return width * height * depth;
+    }
+}
+
+class BoxWeight extends Box2 {
+    double weight;
+
+    BoxWeight(double w, double h, double d, double m) {
+        width = w;
+        height = h;
+        depth = d;
+        weight = m;
+    }
 }
