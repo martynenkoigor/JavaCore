a960947ee92decf5ee65c26ac76ca5f1528c8c00
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-26 15:50:20 +0200

    Add class Box2

--- src/main/com/martynenkoigor/javacore/chapter07/Box2.java
+++ src/main/com/martynenkoigor/javacore/chapter07/Box2.java
@@ -54,3 +54,27 @@ class BoxWeight extends Box2 {
         weight = m;
     }
 }
+
+class Shipment extends BoxWeight {
+    double cost;
+
+    Shipment(double w, double h, double d, double m, double c) {
+        super(w, h, d, m);
+        cost = c;
+    }
+
+    Shipment(Shipment ob) {
+        super(ob);
+        cost = ob.cost;
+    }
+
+    Shipment() {
+        super();
+        cost = -1;
+    }
+
+    Shipment(double len, double m, double c) {
+        super(len, m);
+        cost = c;
+    }
+}
