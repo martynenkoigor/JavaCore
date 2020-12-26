371950fa9deb6eb819c67c1348dfa58e9f026367
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-26 12:41:46 +0200

    Add class Outer

--- src/main/com/martynenkoigor/javacore/chapter06/Outer.java
+++ src/main/com/martynenkoigor/javacore/chapter06/Outer.java
@@ -1,4 +1,23 @@
 package com.martynenkoigor.javacore.chapter06;
 
 public class Outer {
+    int outer_x = 100;
+
+    void test() {
+        Inner inner = new Inner();
+        inner.display();
+    }
+
+    public class Inner {
+        void display() {
+            System.out.println("вывoд: outer х = " + outer_x);
+        }
+    }
+}
+
+class InnerClassdemo {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        outer.test();
+    }
 }
