2e3baba780c072bc913e30e8a00560fc433b7e8e
Martynenko Igor <martynenko.igor2001@gmail.com>
2020-12-26 15:06:38 +0200

    Add class Box2

--- src/main/com/martynenkoigor/javacore/chapter07/Box2.java
+++ src/main/com/martynenkoigor/javacore/chapter07/Box2.java
@@ -35,9 +35,22 @@ class BoxWeight extends Box2 {
     double weight;
 
     BoxWeight(double w, double h, double d, double m) {
-        width = w;
-        height = h;
-        depth = d;
+        super(w, h, d);
+        weight = m;
+    }
+
+    BoxWeight(BoxWeight ob) {
+        super(ob);
+        weight = ob.weight;
+    }
+
+    BoxWeight() {
+        super();
+        weight = -1;
+    }
+
+    BoxWeight(double len, double m) {
+        super(len);
         weight = m;
     }
 }
