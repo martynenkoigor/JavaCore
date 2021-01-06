ba6b23fd535cb1cffd0c09b89b53fdb20b705981
Martynenko Igor <martynenko.igor2001@gmail.com>
2021-01-06 14:47:08 +0200

    Add class Exc1

--- src/main/com/martynenkoigor/javacore/chapter09/Exc1.java
+++ src/main/com/martynenkoigor/javacore/chapter09/Exc1.java
@@ -1,4 +1,11 @@
 package com.martynenkoigor.javacore.chapter09;
 
 public class Exc1  {
+    static void subroutine() {
+        int d = 0;
+        int a = 10 / d;
+    }
+    public static void main(String[] args) {
+        Exc1.subroutine();
+    }
 }
