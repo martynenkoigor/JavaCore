a3a9de1ae54405c95e263d83d9a3d1ec046b8bcd
Martynenko Igor <martynenko.igor2001@gmail.com>
2021-01-07 13:48:22 +0200

    Add class ThrowsDemo

---
+++ src/main/com/martynenkoigor/javacore/chapter09/ThrowsDemo.java
@@ -0,0 +1,15 @@
+package com.martynenkoigor.javacore.chapter09;
+
+public class ThrowsDemo {
+    static void throwOne() throws IllegalAccessException {
+        System.out.println("B теле метода throwOne().");
+        throw new IllegalAccessException("дeмoнcтpaция");
+    }
+    public static void main(String[] args) {
+        try {
+            throwOne();
+        } catch (IllegalAccessException e) {
+            System.out.println("Пepexвaчeнo исключение : " + e);
+        }
+    }
+}
