a38dddb8d53fce23677c7edb1d86f19410c462da
Martynenko Igor <martynenko.igor2001@gmail.com>
2021-01-13 14:28:40 +0200

    Add class Wrap

---
+++ src/main/com/martynenkoigor/javacore/chapter11/Wrap.java
@@ -0,0 +1,11 @@
+package com.martynenkoigor.javacore.chapter11;
+
+public class Wrap {
+    public static void main(String[] args) {
+        Integer iOb = new Integer(100);
+
+        int i = iOb.intValue();
+
+        System.out.println(i + " " + iOb);
+    }
+}
