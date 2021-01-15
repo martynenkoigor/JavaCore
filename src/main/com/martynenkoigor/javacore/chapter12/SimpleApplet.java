e2cc25d8d19dd8c8b35fcd08f89eeba694adddb5
Martynenko Igor <martynenko.igor2001@gmail.com>
2021-01-15 16:09:53 +0200

    Add class SimpleApplet

---
+++ src/main/com/martynenkoigor/javacore/chapter12/SimpleApplet.java
@@ -0,0 +1,10 @@
+package com.martynenkoigor.javacore.chapter12;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class SimpleApplet extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("Пpocтeйwий аплет", 20, 20);
+    }
+}
