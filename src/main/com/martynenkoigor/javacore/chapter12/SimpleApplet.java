484b2e3a84f6460bcd2aa8e487b967c3e3162df7
Martynenko Igor <martynenko.igor2001@gmail.com>
2021-01-16 13:01:23 +0200

    Add class NativeDemo

--- src/main/com/martynenkoigor/javacore/chapter12/SimpleApplet.java
+++ src/main/com/martynenkoigor/javacore/chapter12/SimpleApplet.java
@@ -2,7 +2,10 @@ package com.martynenkoigor.javacore.chapter12;
 
 import java.applet.Applet;
 import java.awt.*;
-
+/*
+<applet code="SimpleApplet" width=200 height=60>
+</applet>
+*/
 public class SimpleApplet extends Applet {
     public void paint(Graphics g) {
         g.drawString("Пpocтeйwий аплет", 20, 20);
