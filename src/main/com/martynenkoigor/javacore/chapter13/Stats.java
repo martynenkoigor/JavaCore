d3ab987cefa2fd033aaa7116d89f35cd49bf2464
Martynenko Igor <martynenko.igor2001@gmail.com>
2021-01-17 16:24:52 +0200

    Add class Stats

---
+++ src/main/com/martynenkoigor/javacore/chapter13/Stats.java
@@ -0,0 +1,34 @@
+package com.martynenkoigor.javacore.chapter13;
+
+import java.io.InputStreamReader;
+
+public class Stats<T extends Number> {
+    T[] nums;
+
+    Stats(T[] o) {
+        nums = o;
+    }
+
+    double average() {
+        double sum = 0.0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i].doubleValue();
+        }
+        return sum / nums.length;
+    }
+}
+
+class BoundsDemo {
+    public static void main(String[] args) {
+        Integer inums[] = {1, 2, 3, 4, 5};
+        Stats<Integer> iOb = new Stats<Integer>(inums);
+        double v = iOb.average();
+        System.out.println("Cpeднee значение iob равно " + v);
+
+        Double dnums[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+        Stats<Double> dOb = new Stats<Double>(dnums);
+        double w = dOb.average();
+        System.out.println("Cpeднee значение iob равно " + w);
+    }
+}
