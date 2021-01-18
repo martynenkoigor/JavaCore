5b161cb5a241c12a5fbfa6b62825eb3111cda251
Martynenko Igor <martynenko.igor2001@gmail.com>
2021-01-18 17:38:15 +0200

    Add class MakeString

---
+++ src/main/com/martynenkoigor/javacore/chapter15/MakeString.java
@@ -0,0 +1,12 @@
+package com.martynenkoigor.javacore.chapter15;
+
+public class MakeString {
+    public static void main(String[] args) {
+        char c[] = {'J', 'a', 'v', 'a'};
+
+        String s1 = new String(c);
+        String s2 = new String(s1);
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
