f8d70d61b2dce4f043d901528ba5901f0a5aa9f6
Martynenko Igor <martynenko.igor2001@gmail.com>
2021-01-19 17:37:01 +0200

    Add class Angles

---
+++ src/main/com/martynenkoigor/javacore/chapter16/Angles.java
@@ -0,0 +1,11 @@
+package com.martynenkoigor.javacore.chapter16;
+
+public class Angles {
+    public static void main(String[] args) {
+        double theta = 120.0;
+        System.out.println(theta + " градусов равно " + Math.toRadians(theta) + "радиан ");
+
+        theta = 1.312;
+        System.out.println(theta + " радиан равно " + Math.toRadians(theta) + "градусов ");
+    }
+}
