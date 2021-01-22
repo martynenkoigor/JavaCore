cd2a12c69b736c8c3ccb860ce628f1fcbd5dc9a5
Martynenko Igor <martynenko.igor2001@gmail.com>
2021-01-22 17:14:51 +0200

    Add class ArrayListDemo

---
+++ src/main/com/martynenkoigor/javacore/chapter17/ArrayListDemo.java
@@ -0,0 +1,30 @@
+package com.martynenkoigor.javacore.chapter17;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList<String>();
+
+        System.out.println("Начальный размер списочного массива al : " + al.size());
+
+        al.add("C");
+        al.add("A");
+        al.add("E");
+        al.add("B");
+        al.add("D");
+        al.add("F");
+        al.add(1, "A2");
+
+        System.out.println("Размер списочного массива al после ввода элементов : " + al.size());
+
+        System.out.println("Coдepжимoe списочного массива al: " + al);
+
+        al.remove("F");
+        al.remove(2);
+
+        System.out.println("Размер сnисочного массива al после удаления элементов : " + al.size());
+
+        System.out.println("Coдepжимoe списочного массива al: " + al);
+    }
+}
