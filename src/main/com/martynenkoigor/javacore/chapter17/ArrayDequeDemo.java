bfb6e018d904864fc3c6d6458631a2813ba6b794
Martynenko Igor <martynenko.igor2001@gmail.com>
2021-01-24 13:45:50 +0200

    Add class ArrayDequeDemo

---
+++ src/main/com/martynenkoigor/javacore/chapter17/ArrayDequeDemo.java
@@ -0,0 +1,22 @@
+package com.martynenkoigor.javacore.chapter17;
+
+import java.util.ArrayDeque;
+
+public class ArrayDequeDemo {
+    public static void main(String[] args) {
+        ArrayDeque<String> adq = new ArrayDeque<String>();
+
+        adq.push("A");
+        adq.push("B");
+        adq.push("D");
+        adq.push("E");
+        adq.push("F");
+
+        System.out.print("Извлeчeниe из стека: ");
+
+        while (adq.peek() != null)
+            System.out.print(adq.pop() + " ");
+
+        System.out.println();
+    }
+}
