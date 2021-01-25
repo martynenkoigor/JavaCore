998d268f6e5d22964a57f0ead0ace354394369a4
Martynenko Igor <martynenko.igor2001@gmail.com>
2021-01-25 16:05:02 +0200

    Add class HashMapDemo

---
+++ src/main/com/martynenkoigor/javacore/chapter17/HashMapDemo.java
@@ -0,0 +1,29 @@
+package com.martynenkoigor.javacore.chapter17;
+
+import java.util.*;
+
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+        HashMap<String, Double> hm = new HashMap<String, Double>();
+
+        hm.put("Джoн Доу", new Double(3434.34));
+        hm.put("Toм Смит", new Double(123.22));
+        hm.put("Джейн Бейкер", new Double(1378.00));
+        hm.put("Toд Холл", new Double(99.22));
+        hm.put("Paльф Смит", new Double(-19.08));
+
+        Set<Map.Entry<String, Double>> set = hm.entrySet();
+
+        for (Map.Entry<String, Double> me : set) {
+            System.out.print(me.getKey() + ": ");
+            System.out.println(me.getValue());
+        }
+
+        System.out.println();
+
+        double balance = hm.get("Джoн Доу");
+        hm.put("Джoн Доу", balance + 1000);
+        System.out.println("Hoвый остаток на счете Джона Доу: " + hm.get("Джoн Доу"));
+    }
+}
