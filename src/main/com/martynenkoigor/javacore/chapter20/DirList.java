0b15cbe03b23dbdf1918bff5499999aee533d890
Martynenko Igor <martynenko.igor2001@gmail.com>
2021-02-09 15:58:16 +0200

    Add class DirList

---
+++ src/main/com/martynenkoigor/javacore/chapter20/DirList.java
@@ -0,0 +1,25 @@
+package com.martynenkoigor.javacore.chapter20;
+
+import java.io.File;
+
+public class DirList {
+    public static void main(String[] args) {
+        String dirname = "/Users/igor";
+        File f1 = new File(dirname);
+        if (f1.isDirectory()) {
+            System.out.println("Catalog: " + dirname);
+            String s[] = f1.list();
+
+            for (int i = 0; i < s.length; i++) {
+                File f = new File(dirname + "/" + s[i]);
+                if (f.isDirectory()) {
+                    System.out.println(s[i] + "is catalog");
+                } else {
+                    System.out.println(s[i] + "is file");
+                }
+            }
+        } else {
+            System.out.println(dirname + " is not catalog");
+        }
+    }
+}
